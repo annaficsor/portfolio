52200b8e1e2c864a5c38ff8d42ef95389881cdf8
Anna Ficsor <ficsor.anna@gmail.com>
2019-01-19 11:54:54 +0100

    Updates

---
+++ precache-manifest.57423115aab0e3e75954e4f0c9275d9b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef1af5e7cd97309593f966ed27b79863",
+    "url": "/portfolio/static/media/profile_green@3x.ef1af5e7.png"
+  },
+  {
+    "revision": "abd6316958f8b3880a83",
+    "url": "/portfolio/static/css/main.ef2925fe.chunk.css"
+  },
+  {
+    "revision": "e6de05fa279c92775ea8",
+    "url": "/portfolio/static/js/1.e6de05fa.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5f734c43245e924544f74433b674fd77",
+    "url": "/portfolio/static/media/github.5f734c43.svg"
+  },
+  {
+    "revision": "9bc034b5210d8750c53f64db4565710b",
+    "url": "/portfolio/static/media/email.9bc034b5.svg"
+  },
+  {
+    "revision": "19e44e68df87a9603038543e6183cd0b",
+    "url": "/portfolio/static/media/codewars.19e44e68.svg"
+  },
+  {
+    "revision": "5affa6e88d65919901ecacba62439456",
+    "url": "/portfolio/static/media/linkedin.5affa6e8.svg"
+  },
+  {
+    "revision": "19b905e67d0dfc10dd0b1d4b23ed6928",
+    "url": "/portfolio/static/media/profile@3x.19b905e6.png"
+  },
+  {
+    "revision": "abd6316958f8b3880a83",
+    "url": "/portfolio/static/js/main.abd63169.chunk.js"
+  },
+  {
+    "revision": "66711912d17834288a8378ec9db7feb5",
+    "url": "/portfolio/static/media/headerbig.66711912.svg"
+  },
+  {
+    "revision": "6d39ded66af15d10b5ab6504a62a5da6",
+    "url": "/portfolio/static/media/footer.6d39ded6.svg"
+  },
+  {
+    "revision": "4f998c335dbbca43330d51cfdc4a480e",
+    "url": "/portfolio/static/media/menu.4f998c33.svg"
+  },
+  {
+    "revision": "075f7eee2f436163b13ce73be334b55d",
+    "url": "/portfolio/static/media/arrow-down.075f7eee.svg"
+  },
+  {
+    "revision": "5e7daade51c2d50c6bc89a943c17c109",
+    "url": "/portfolio/static/media/profile.5e7daade.png"
+  },
+  {
+    "revision": "003e4d0a0efdb525037e02ffaad22db5",
+    "url": "/portfolio/static/media/profile@2x.003e4d0a.png"
+  },
+  {
+    "revision": "cae02e6b35ae36aa0767a2663a5b49e5",
+    "url": "/portfolio/static/media/profile_green.cae02e6b.png"
+  },
+  {
+    "revision": "75330120713f624500537dedb58f4a8f",
+    "url": "/portfolio/static/media/profile_green@2x.75330120.png"
+  },
+  {
+    "revision": "cfc78710faa9417e881937cc997e6691",
+    "url": "/portfolio/index.html"
+  }
+];
