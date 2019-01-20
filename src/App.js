0bbd9d98fe7fc44896e8b09dc895ed51df70f52d
Anna Ficsor <ficsor.anna@gmail.com>
2019-01-20 19:31:05 +0100

    Modify Link

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             <Switch>
               <Route exact path='/' component={Main}/>
               <Route exact path='/about' component={About}/>
-              <Route exact path='/:index/:project' component={CaseStudy}/>
+              <Route exact path='/:index' component={CaseStudy}/>
             </Switch>
           </ScrollToTop>
         </Analytics>
--- src/Projects.js
+++ src/Projects.js
@@ -23,7 +23,7 @@ class Projects extends React.Component {
                     <h1 className="pt-4">{project.name}</h1>
                     <p>{project.descript}</p>
                     <Link
-                    to={`/${index}/${project.linkname}`}
+                    to={`/${index}`}
                     ><button className="mb-2">Read the case study &#62;&#62; </button></Link>
                     {project.github && (
                       <div>
